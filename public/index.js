8ba6368d373549c3da11afbfeac15771e622b9f7
Maya Ziv <mziv@cs.stanford.edu>
2020-06-24 09:42:40 -0700

    Hacky fix for video autoplay

--- public/index.js
+++ public/index.js
@@ -8,6 +8,10 @@ const main = () => {
   let gateForm = document.querySelector("#gateForm");
   gateForm.onsubmit = checkGateForm;
 
+  let vid = document.getElementById("intro");
+  
+  setTimeout(function(){ vid.muted = false; }, 1500);
+
   // setTimeout(function(){ gateForm.classList.remove('hide'); }, 1000);
   setTimeout(function(){ gateForm.classList.remove('hide'); }, 67000);
 }
